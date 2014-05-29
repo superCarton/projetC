bd41fe507ed3b534e0ee22fcc4f89d1025956447
Romain GUILLOT <guillot.romain@etu.unice.fr>
2014-05-29 15:12:38 +0200

    stdio.c

---
+++ projetC/stdio.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int _flsbuf(unsigned char c, FILE *f);
+extern int _filbuf(FILE * f);
+FILE *fopen(const char *path, const char *mode);
+void setbuf(FILE *stream, char *buf);
+int setvbuf(FILE *stream, char *buf, int mode, int size);
+int fclose(FILE *fp);
